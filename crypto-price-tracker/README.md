c97776174d71c3ce0d76a9c96409870aeddf3116
home12 <home12@Microsoft-Surface-Pro.local>
2024-06-30 19:34:30 -0400

    Following the tutorial

---
+++ crypto-price-tracker/README.md
@@ -0,0 +1,12 @@
+### Credit 
+This project is a practice project following the video tutorial guide 'Angular Project - Cryptocurrency price tracker | Angular HTTP Client | Angular Material UI' provided by Let's Program Channel.
+
+YT Link: https://www.youtube.com/watch?v=vb6b67HiuC8
+
+Github: https://github.com/yshashi/crypto-checker-yt
+
+
+
+#### Changes added
+Added more currency in the dropdown menu.
+Made the values in coin-detail page more dynamic to accomodate other currencies.
---
+++ crypto-price-tracker/src/app/coin-list/coin-list.component.ts
@@ -0,0 +1,67 @@
+import { Component, ViewChild } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { Router } from '@angular/router';
+import { CurrencyService } from '../service/currency.service';
+
+
+@Component({
+  selector: 'app-coin-list',
+  templateUrl: './coin-list.component.html',
+  styleUrls: ['./coin-list.component.scss']
+})
+export class CoinListComponent {
+
+  bannerData: any = [];
+  currency: string = "USD";
+  dataSource!: MatTableDataSource<any>;
+  displayedColumns: string[] = ['symbol', 'current_price', 'price_change_percentage_24h', 'market_cap'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(private api: ApiService, private router: Router, private currencyService: CurrencyService) { }
+
+  ngOnInit(): void {
+    this.getBannerData();
+    this.getAllData();
+    this.currencyService.getCurrency()
+    .subscribe(val=>{
+      this.currency = val;
+      this.getAllData();
+      this.getBannerData();
+    });
+  }
+
+  getBannerData() {
+    this.api.getTrandingCurrency(this.currency)
+      .subscribe(res => {
+        console.log(res);
+        this.bannerData = res;
+      })
+  }
+
+  getAllData() {
+    this.api.getCurrency(this.currency)
+      .subscribe(res => {
+        console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  goToDetails(row: any){
+    this.router.navigate(['coin-detail',row.id])
+  }
+}
